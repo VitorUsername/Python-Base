e501e6bbdfd2fb5b631a6ea31d65f8cca54dc70c
VitorUsername <vitorsiegergrafico@gmail.com>
2025-02-16 18:55:30 -0400

    atualizando

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
+#!/usr/bin/env python #boas práticas
+
+# SHEBANG é um comentário especial que será executdado antes do meu programa iniciar,
 print("Ola,  Mundo!".upper()) # .upper() usado para deixar as letras de uma string maiúscula
